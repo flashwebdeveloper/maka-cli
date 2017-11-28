4fced140253629153404c9755e4bc5dba77555ff
Matthew Campbell <mjmcee@gmail.com>
2017-11-28 14:14:25 -0500

    ssr option... stay tuned

--- lib/templates/app/imports/startup/client/index.js.jsx
+++ lib/templates/app/imports/startup/client/index.js.jsx
@@ -1,5 +1,5 @@
 /** @namespace Client */
-import Routes from './routes.jsx';
+<% if (config.engines.ssr === 'true') { %>import Routes from '../lib/routes.jsx';<% } else { %> import Routes from './routes.jsx'; <% } %>
 
 import React from 'react';
 import ReactDOM from 'react-dom';
